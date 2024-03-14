71c04f4bc51c151030454fd4703f8f3cd2aa94a7
Hassan Syyid <hassansyyid@gmail.com>
2024-03-14 00:03:55 -0400

    fix strict address fields

--- target_shopify_v2/mapping.py
+++ target_shopify_v2/mapping.py
@@ -47,8 +47,8 @@ class UnifiedMapping:
         address = {}
         countries = self.read_json_file(f"countries.json")
         for key in address_mapping.keys():
-            address[address_mapping[key]] = addresses[key]
-        if len(address["countryCode"]) == 3:
+            address[address_mapping[key]] = addresses.get(key)
+        if len(address.get("countryCode","")) == 3:
             address["countryCode"] = countries[address["countryCode"]]
         payload[type] = address
         return payload
