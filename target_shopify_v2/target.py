43206ecbb72e0205bc3df21d1647b56c18a4f159
Otavio Trentin <otavio_trentin@hotmail.com>
2026-07-28 11:13:52 -0400

    HGI-10841: migrate to HotglueSingerSDK

--- target_shopify_v2/target.py
+++ target_shopify_v2/target.py
@@ -1,7 +1,7 @@
 """TargetShopifyV2 target class."""
 
-from singer_sdk import typing as th
-from target_hotglue.target import TargetHotglue
+from hotglue_singer_sdk import typing as th
+from hotglue_singer_sdk.target_sdk.target import TargetHotglue
 from target_shopify_v2.sinks import (
     SalesOrdersSink,
     FulfillmentsSink,
